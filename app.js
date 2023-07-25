555094eec75746c1031d342f4a20c0b5813449a6
jonasjrn <jonasjrn@hotmail.com>
2023-07-25 10:09:32 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log ("aplicacão está se conectando ao banco de dados")
+// mais um console log de testes:
+console.log("Enviado para o GitHub")
